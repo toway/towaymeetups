bebf89110471d113f342710305e46ef538b9504e
gan <wei_shenggan@szhnd.com>
2014-01-22 18:03:52 +0800

    add all users and cannot run

--- mba/resources.py
+++ mba/resources.py
@@ -48,7 +48,36 @@ from kotti.sqla import JsonType
 from kotti.sqla import MutationList
 from kotti.sqla import NestedMutationDict
 from kotti.util import ViewLink
-from kotti.util import _
+#from kotti.util import _
 from kotti.util import camel_case_to_name
 from kotti.util import get_paste_items
+from kotti.resources import Document
+
+from mba import _
+
+class ActivityStatus:
+    DRAFT,PUBLIC,FINISH,CANCEL=0,1,2,3
+
+# Extent from document ?
+# How to make a test?
+class Activity(Document):
+    id = Column('id', Integer, ForeignKey('documents.id'), primary_key=True)
+    status = Column(Integer(), nullable=False)
+
+    type_info = Document.type_info.copy(
+        name=u'Activity',
+        title=_(u'Activity'),
+        add_view=u'add_activity',
+        addable_to=[u'Activity'],
+        )
+
+# For who participate what activity
+# many to many relationship
+# refer to many relationship of (tag <--> content)
+participate = Table(
+    'participate', Base.metadata,
+    Column('mbauser_id', Integer, ForeignKey('mbauser.id')),
+    Column('')
+class Participate(Base):
+    id
 
--- mba/security.py
+++ mba/security.py
@@ -25,9 +25,13 @@ from kotti.sqla import JsonType
 from kotti.util import _
 from kotti.util import request_cache
 from kotti.util import DontCache
-from kotti.security import Principal, Principals
+from kotti.security import Principals, get_principals
+
+class UserType:
+    USER_BASE, STUDENT, TEACHER, PROFESSOR, COMPANY = 0,1,2,3,4
 
 #TODO extent Principal or create new one with ForeignKey('Principal.id') ?
+'''
 class MbaPrincipal(Principal):
 
     __tablename__ = 'mbausers'
@@ -42,9 +46,107 @@ class MbaPrincipal(Principal):
         super(MbaPrincipal, self).__init__(name, password, active, confirm_token,
                  title, email, groups)
         self.class_number = class_number
+'''
+#This is a base class for all users
+class MbaUser(Base):
+    __tablename__ = 'mbauser'
+    __mapper_args__ = dict(
+        order_by='mbauser.name',
+        )
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(100), unique=True)
+    password = Column(Unicode(100))
+    active = Column(Boolean)
+    confirm_token = Column(Unicode(100))
+    title = Column(Unicode(100), nullable=False)
+    email = Column(Unicode(100), unique=True)
+    groups = Column(JsonType(), nullable=False)
+    creation_date = Column(DateTime(), nullable=False)
+    last_login_date = Column(DateTime())
+
+    #TODO how to set a picture?
+
+    #The real user type(Student, Teacher, Professor or Company?
+    obj_type = Column(Integer(), nullable=False)
+
+    def __init__(self, name, password=None, active=True, confirm_token=None,
+                 title=u"", email=None, groups=(), obj_type=UserType.USER_BASE):
+        self.name = name
+        if password is not None:
+            password = get_principals().hash_password(password)
+        self.password = password
+        self.active = active
+        self.confirm_token = confirm_token
+        self.title = title
+        self.email = email
+        self.groups = groups
+        self.creation_date = datetime.now()
+        self.last_login_date = None
+        self.obj_type = obj_type
+
+    def __repr__(self):  # pragma: no cover
+        return '<MbaUser %r>' % self.name
+    '''
+    def real_user(self):
+        pass
+    '''
+
+class Student(MbaUser):
+    __tablename__ = 'student'
+    __mapper_args__ = dict(
+        order_by='student.name',
+        )
+    id = Column('id', Integer, ForeignKey('mbauser.id'), primary_key=True)
+    #TODO for the other column
+
+    def __init__(self, name, password=None, active=True, confirm_token=None,
+                 title=u"", email=None, groups=(), obj_type=UserType.USER_BASE):
+        super(Student, self).__init__(name, password, active, confirm_token,
+                 title, email, groups, UserType.TEACHER)
+
+class Teacher(MbaUser):
+    __tablename__ = 'teacher'
+    __mapper_args__ = dict(
+        order_by='teacher.name',
+        )
+    id = Column('id', Integer, ForeignKey('mbauser.id'), primary_key=True)
+    #TODO for the other column
+
+    def __init__(self, name, password=None, active=True, confirm_token=None,
+                 title=u"", email=None, groups=(), obj_type=UserType.USER_BASE):
+        super(Teacher, self).__init__(name, password, active, confirm_token,
+                 title, email, groups, UserType.TEACHER)
+
+class Professor(MbaUser):
+    __tablename__ = 'professor'
+    __mapper_args__ = dict(
+        order_by='professor.name',
+        )
+    id = Column('id', Integer, ForeignKey('mbauser.id'), primary_key=True)
+    #TODO for the other column
+
+    def __init__(self, name, password=None, active=True, confirm_token=None,
+                 title=u"", email=None, groups=(), obj_type=UserType.USER_BASE):
+        super(Professor, self).__init__(name, password, active, confirm_token,
+                 title, email, groups, UserType.PROFESSOR)
+
+# Implement in the next version
+class Company(MbaUser):
+    __tablename__ = 'company'
+    __mapper_args__ = dict(
+        order_by='company.name',
+        )
+    id = Column('id', Integer, ForeignKey('mbauser.id'), primary_key=True)
+    #TODO for the other column
+
+    def __init__(self, name, password=None, active=True, confirm_token=None,
+                 title=u"", email=None, groups=(), obj_type=UserType.USER_BASE):
+        super(Company, self).__init__(name, password, active, confirm_token,
+                 title, email, groups, UserType.COMPANY)
 
 class MbaPrincipals(Principals):
-    factory = MbaPrincipal
+    factory = MbaUser
 
 def principals_factory():
     return MbaPrincipals()
