0e60c68274a99262783635a135b65581328243d4
janson <gandancing@gmail.com>
2014-04-14 00:49:26 +0800

    redefined the resume_edit

--- mba/static/js/mbaForm.js
+++ mba/static/js/mbaForm.js
@@ -15,6 +15,8 @@ var mbaForm  = {
       $(function() {
         if (!mbaForm_loaded) {
             mbaForm.processCallbacks();
+	    updateInputs('.in_person_info');
+	    updateTables('.sp_person_info');
             mbaForm.focusFirstInput();
             mbaForm_loaded = true;
       }});
@@ -38,6 +40,7 @@ var mbaForm  = {
         }
         );
     },
+
     focusFirstInput: function (el) {
         el = el || document.body;
         var input = $(el).find(':input')
@@ -59,3 +62,99 @@ var mbaForm  = {
     }
 
 };
+
+function updateInputs(class_name, obj) {
+	if(typeof arguments[1] == 'undefined') {
+		obj = {};
+	}
+	$(class_name).each(function (){
+		el = $(this);
+		id = el.attr('id');
+		data_type = el.attr('data-type');
+		if(typeof data_type == 'undefined') {
+                	//console.log(id+"|UNDEF|"+data_type);
+			if(id in obj) {
+				el.val(obj[id]);
+			}
+		} else {
+                	//console.log(id+"|DEF|"+data_type);
+			if(data_type == 'date') {
+				if(id in obj) {
+					el.val(obj[id]);
+				} else {
+					el.datepicker({dateFormat:'yy-mm-dd'});
+				}
+			} else if(data_type == 'radio') {
+				el_name = "input[name=" + el.attr('name') + "]";
+				if(id in obj) {
+					arg = obj[id] + "";
+					$(el_name).each(function() {
+						if(arg == $(this).attr("value") ) {
+							$(this).prop("checked","checked");
+						}
+					});
+				} else {
+					$(el_name).each(function() {
+						if($(this).attr("data-val") == $(this).attr("value") ) {
+							$(this).prop("checked","checked");
+						}
+					});
+				}
+			} else if(data_type == 'select') {
+				if(id in obj) {
+					el.val(obj[id]+"");
+				} else {
+					el.val(el.attr('data-val'));
+				}
+			}
+		}
+	});
+}
+
+function updateTables(class_name, obj) {
+	if(typeof arguments[1] == 'undefined') {
+		obj = {};
+	}
+	$(class_name).each(function () {
+		sp_id = $(this).attr('id');
+		id = sp_id.substring(sp_id.indexOf('_')+1)
+		sp_obj = $('#'+sp_id)
+		data_type = sp_obj.attr('data-type');
+		if(typeof data_type == 'undefined') {
+			data_type = 'undefined';
+		}
+		value = '';
+		if(id in obj) {
+			value = obj[id];
+			if(data_type == 'undefined') {
+				sp_obj.html(value);
+				return;//like continue; return false;->like break;
+			}
+		} else {
+			value = sp_obj.attr('data-val');
+		}
+		if(data_type != 'undefined') {
+			values = null;
+			data_sel = sp_obj.attr('data-sel');
+			if(typeof data_sel == 'undefined') {
+				values = {};
+				$("#" + id + " > option").each(function() {
+					try {
+						v = parseInt(this.value);
+						values[v] = this.text;
+					}
+					catch(err) {
+					}
+				});
+			} else {
+				values = data_sel.split('|');
+			}
+			try {
+				v = parseInt(value);
+				sp_obj.html(values[v]);
+			}
+			catch(err) {
+			}
+		}
+	});
+}
