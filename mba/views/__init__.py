914936c7cc3f8aef7aaa69c7b156bc6547dc822c
alex-chan <czzsunset@gmail.com>
2014-06-19 23:05:52 +0800

    Add unfinished url /meetup-review.

--- mba/views/__init__.py
+++ mba/views/__init__.py
@@ -6,6 +6,7 @@ def includeme(config):
 
     config.include('mba.views.meetups')
     config.include('mba.views.meetup')
+    config.include('mba.views.meetup_review')
 
     config.include('mba.views.login')
     #config.include('mba.views.logout')
