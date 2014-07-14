9d569dc48d2fc1d3a207961a218b270267feb89e
U-TPLINK-3C38E2A0\root <root@sunset.(none)>
2014-07-14 16:20:27 +0800

    Add widget URLInputWidget

--- mba/views/widget.py
+++ mba/views/widget.py
@@ -4,7 +4,5 @@ from deform.widget import Widget, TextInputWidget
 
 
 class URLInputWidget(TextInputWidget):
-    def serialize(self, field, cstruct, **kw):
-        urlinput = TextInputWidget.serialize(self, field, cstruct, **kw)
-        return "http://xxx/" + urlinput
+    template = "urlinput"
 
