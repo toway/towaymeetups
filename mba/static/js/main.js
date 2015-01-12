38c39c975a8dc793498c8bac100ca00a585282c0
alex-chan <czzsunset@gmail.com>
2015-01-12 19:41:55 +0800

    完成找找看选择的UI

--- mba/static/js/main.js
+++ mba/static/js/main.js
@@ -1,1 +1,12 @@
 function isMobile(){if(/android|blackberry|iphone|ipad|ipod|iemobile|wap|mobile/i.test(navigator.userAgent.toLowerCase())){return true;}else{return false;}}
+$(function(){
+    $("#find-sth a").click(function(){
+        var wclicked = $(this);
+
+        var action = (wclicked.attr('data-search-type'));
+
+        $("#find-sth").attr("action", action );
+        $("#find-sth input").attr("placeHolder", "找找看("+wclicked.text()+")");
+
+    })
+});
